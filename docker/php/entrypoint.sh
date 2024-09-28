4c0f6ca2d79eec2d37c894c7944b259784ebe4f9
Ikaro C. Laborda <iclaborda@msn.com>
2024-09-28 15:29:09 +0100

    build: infrastructure optimizations

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -4,13 +4,18 @@ set -ex
 # Check if the vendor directory exists; if not, run composer install
 if [ ! -d "/var/www/crypto/vendor" ]; then
   echo "Vendor directory not found. Running composer install..."
-  bash -c "composer install --no-dev --optimize-autoloader --no-interaction --no-suggest"
+
+  # Set the memory limit for Composer to unlimited
+  export COMPOSER_MEMORY_LIMIT=-1
+
+  # Run composer install with production flags
+  bash -c "composer install --optimize-autoloader --no-interaction --no-suggest"
 else
   echo "Vendor directory found. Skipping composer install."
 fi
 
 # Clear cached files
-bash -c "php artisan optimize:clear"
+bash -c "php artisan cache:clear"
 
 # Run the queue worker in the background
 bash -c "php artisan queue:work --daemon &"
