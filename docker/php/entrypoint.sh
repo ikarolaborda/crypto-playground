d8c9e902852c30840aaeee8016f17648be0e20d9
Ikaro C. Laborda <iclaborda@msn.com>
2024-09-28 15:19:17 +0100

    build: optimizing php entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -ex
 
+# Check if the vendor directory exists; if not, run composer install
+if [ ! -d "/var/www/crypto/vendor" ]; then
+  echo "Vendor directory not found. Running composer install..."
+  bash -c "composer install --no-dev --optimize-autoloader --no-interaction --no-suggest"
+else
+  echo "Vendor directory found. Skipping composer install."
+fi
+
 # Clear cached files
 bash -c "php artisan optimize:clear"
 
