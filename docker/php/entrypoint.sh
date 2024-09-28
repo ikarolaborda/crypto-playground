1c99f21685f77ce548d423175cb15828076127ae
Ikaro C. Laborda <iclaborda@msn.com>
2024-09-28 15:07:14 +0100

    build: remove composer.lock for consistent dependency version and fixing entrypoints

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -ex
 
-# Install composer dependencies
-bash -c "composer install --no-interaction --no-progress --no-suggest"
-
 # Clear cached files
 bash -c "php artisan optimize:clear"
 
